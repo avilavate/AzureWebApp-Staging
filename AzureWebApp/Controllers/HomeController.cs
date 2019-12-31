4594d56053c96cbfe0ab047b1f041c8886ba24b3
Avil Avate <avilavate123@gmail.com>
2019-12-31 22:27:31 +0530

    Azure webapp staging branch

---
+++ AzureWebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureWebApp.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration congigurattion { get; private set; }
+
+        public HomeController(IConfiguration configuration )
+        {
+            this.congigurattion = configuration;
+        }
+        public IActionResult Index()
+        {
+            var model = this.congigurattion["Greetings"];
+
+            return View("index",model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
